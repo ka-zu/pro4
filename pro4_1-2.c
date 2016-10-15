814d8c1bc1373c0c23f33c6df5d6cd4b2a0a76ae
kazu <sprite13return@gmail.com>
2016-10-15 20:38:22 +0900

    課題完成
    pro4_1-2.c

--- pro4_1-2.c
+++ pro4_1-2.c
@@ -1,32 +1,46 @@
 //j1422 土井　kadai2-1
 #include <stdio.h>
 #include <stdlib.h>
-#define stach_type char
+#define stack_type char
+#define stack_size 100
 #include "stack.h"
 int main()
 {
-  int ch, pch, small;
-  small = 0;
-  init_stack();
-  fprintf(stdout, "Please enter English text.:\n");
-  pch = "";
-  while((ch = getchar()) != '\n')
-  {
-    if(ch != '')
-    {
-      if(ch >= 'a' && ch <= 'z' && pch = ' ')
-      {
-        push(ch);
-      }else{
-        fprintf(stdout, "%c",pop() );
-      }
-    }else{
-      printf(" ");
-    }pch = ch;
-  }
-  while(stack_stock()>0)
-  {
-    fprintf(stdout, "%c",pop() );
-  }
-  fprintf(stdout, "\n");
+	int ch, pch, small;
+	small = 0;
+	init_stack();
+	fprintf(stdout, "Please enter English text:\n");
+	pch = ' ';
+	while((ch = getchar()) != '\n')
+	{
+		if(ch != ' ')
+		{
+			if(ch >= 97 && ch <= 122 && pch == ' ')
+			{
+				small = 1;
+				//push(ch);
+			}else if(pch == ' '){
+				small = 0;
+			}
+
+			if(small==1)
+			{
+				push(ch);
+			}else if(small == 0){
+				printf("%c",ch);
+			}
+
+		}else{
+			while(stack_stock()>0)
+			{
+				fprintf(stdout, "%c",pop() );
+			}
+			printf(" ");
+		}pch = ch;
+	}
+	while(stack_stock()>0)
+	{
+		fprintf(stdout, "%c",pop() );
+	}
+	fprintf(stdout, "\n");
 }
