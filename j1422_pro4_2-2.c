17caeea49521dcd3e6031f02ccfe699a3875df79
kazu <sprite13return@gmail.com>
2016-10-25 22:07:42 +0900

    課題完成
    課題2-2
    frontとrearのみのリングバッファ

---
+++ j1422_pro4_2-2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Q_size 11
+#define Q_type int
+#include "j1422_queue2.h"
+
+int main()
+{
+  int c;
+  int i;
+  int count;
+  int num;
+
+  init_Q();//キューの初期化
+
+  //
+  fprintf(stdout, "Queue size: %d",Q_size-1 );
+  fprintf(stdout, "\nPlease enter text:" );
+
+  //
+  while ((c = getchar()) != '\n') {
+    enqueue(c);
+  }
+
+  printf("Front: %d   Rear: %d",f,r);//先頭　末尾の位置を出力
+  fprintf(stdout, "\nArray Q data: ");
+  for(i = 0;i<=stock_Q();i++)//キューの配列内のデータを出力
+  {
+    fprintf(stderr, "%c ",Q[i]);
+  }
+
+  fprintf(stdout, "\nPlease enter dequeue number: ");
+  scanf("%d",&num );//dequeueする値受け取り,格納
+
+  fprintf(stdout, "Dequeue data: ");
+  for(i=0;i<num;i++)
+  {
+    fprintf(stderr, "%c ",dequeue() );
+  }
+  
+  fprintf(stdout, "\nPlease enter next text(MAX num:%d)",Q_size-stock_Q()-1 );
+  c = getchar();
+  while ((c=getchar()) != '\n') {
+    enqueue(c);
+  }
+
+  fprintf(stderr, "Queue all data: ");
+  while (stock_Q() != 0) {
+    fprintf(stderr, "%c ",dequeue() );
+  }
+
+  fprintf(stderr, "\nArray Q data:");
+  for(i=0;i<Q_size;i++)
+  {
+    fprintf(stderr, "%c ",Q[i]);
+  }
+}
---
+++ j1422_queue2.h
@@ -0,0 +1,55 @@
+Q_type Q[Q_size];
+int f, r;
+
+void init_Q(){
+  f = 0;
+  r = -1;
+}
+
+int stock_Q()
+{
+  if(f==r+1){
+    return 0;
+  }
+  else if(f == r+2){
+	  return Q_size+1;
+  }
+  else if(f==r){
+	  return 1;
+  }
+  else{
+	  return (r-f)+1;
+  }
+}
+
+void enqueue(Q_type d)
+{
+	printf("%c",d);
+	if((r+2)%Q_size == f){
+		printf("Queue is full \n");
+		exit(1);
+	}
+	
+	r = (r+1)%(Q_size-1);
+	Q[r]=d;
+}
+
+Q_type dequeue()
+{
+	int x;
+	if(f==((r+1)%(Q_size-1))){
+		printf("Queue is empty \n");
+		exit(1);
+	}
+	x = Q[f];
+	f=(f+1)%(Q_size-1);
+	return x;
+}
+
+void print_Q()
+{
+	for(f;f<r;f++)
+	{
+		printf("%c",Q[f]);
+	}
+}
