a86345964d3524a7a30cec10369d80cafbfdec5b
kazu <sprite13return@gmail.com>
2016-11-20 09:53:05 +0900

    課題完成
    木構造
    6-1　木構造の構成
    6-2　木構造に値の挿入
    6-3　木構造の値の削除

---
+++ j1422_pro4_kadai6-2.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+  int data;
+  struct node *left;
+  struct node *right;
+}TREE;
+
+TREE *root;
+
+void construct_tree(){
+  TREE *p,*q;
+  int n, y, i, k, noIndex[50];
+  char buf[100];
+  p = (TREE *)malloc( sizeof(TREE) );
+  while(1){
+    fprintf(stdout, "Input data: ");
+    fgets(buf, sizeof(buf), stdin);
+    if(buf[0] == '\n'){
+      return;
+    }
+    i=0;  k=0;
+    if(buf[i] != ' '){
+      noIndex[k++] = i;
+    }
+    while(buf[i] != '\0'){
+      i++;
+      if(buf[i-1] == ' ' && buf[i] != ' ' && buf[i] != '\n'){
+        noIndex[k++] = i;
+      }
+    }
+    if(buf[i-1] != '\n'){
+      fprintf(stderr, "Error:Too long input data\n");
+      exit(1);
+    }
+    //入力されたノードを格納して木を構築する
+    for(i=0;i<k;i++){
+      n = sscanf(&buf[ noIndex[i] ], "%d", &y);
+      if(n != 1){
+        fprintf(stderr, "Error:non-number input data\n");
+        continue;
+      }
+      if (i == 0) {//最初だけ根を構成する
+        q = (TREE *)malloc( sizeof(TREE) );
+        q->data = y;
+        q->left = NULL;
+        q->right = NULL;
+        root = q;
+      }else{//節や葉を構成する
+        p = root;
+        while(p != NULL){
+          if(y < p->data){//入れる値が今見ている値より小さいとき
+            if(p->left != NULL){//左が空いなかったら
+              p = p->left;//左へ移動
+            }
+            else{//空いていたら
+              q = (TREE *)malloc( sizeof(TREE) );
+              q->data = y;
+              q->left = NULL;
+              q->right = NULL;
+              p->left = q;//新しく葉(節)にする
+              break;
+            }
+          }
+          else if(y > p->data){
+            if(p->right != NULL){
+              p = p->right;
+            }
+            else{
+              q = (TREE *)malloc( sizeof(TREE) );
+              q->data = y;
+              q->left = NULL;
+              p->right = NULL;
+              p->right = q;
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void print_tree(int depth, TREE *p){
+  int i;
+  if(p == NULL){
+    return;
+  }
+  //木のデータを出力
+  print_tree(depth+1,p->right);
+  for(i=0;i<depth;i++){
+    fprintf(stdout, "    ");
+  }
+  fprintf(stdout," %4d\n", p->data);
+  print_tree(depth+1, p->left);
+}
+
+void insert_node(int y, TREE *p){
+  TREE *q;
+  //空でないか確認
+  if(p == NULL){
+    fprintf(stderr, "Error:There is no data!");
+    return;
+  }
+  q = (TREE *)malloc( sizeof(TREE) );
+  q->data = y;
+  q->left = NULL;
+  q->right = NULL;
+  if(y < p->data){
+    if(p->left != NULL){
+      insert_node(y,p->left);
+    }else{
+      q = (TREE *)malloc( sizeof(TREE) );
+      q->data = y;
+      q->left = NULL;
+      q->right = NULL;
+      p->left = q;
+    }
+  }
+  else if(y > p->data){
+    if(p->right != NULL){
+      insert_node(y,p->right);
+    }else{
+      q = (TREE *)malloc( sizeof(TREE) );
+      q->data = y;
+      q->left = NULL;
+      q->right = NULL;
+      p->right = q;
+    }
+  }
+}
+
+int main(){
+  int y;
+  root = NULL;
+  construct_tree();
+  print_tree(0,root);
+  //insert data
+  fprintf(stdout, "*******Insert_node*******\n");
+  fprintf(stdout, "Input insert data: ");
+  scanf("%d",&y);
+  insert_node(y,root);
+  print_tree(0,root);
+  return 0;
+}
---
+++ j1422_pro4_kadai6-3.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+  int data;
+  struct node *left;
+  struct node *right;
+}TREE;
+
+TREE *root;
+
+void construct_tree(){
+  TREE *p,*q;
+  int n, y, i, k, noIndex[50];
+  char buf[100];
+  p = (TREE *)malloc( sizeof(TREE) );
+  while(1){
+    fprintf(stdout, "Input data: ");
+    fgets(buf, sizeof(buf), stdin);
+    if(buf[0] == '\n'){
+      return;
+    }
+    i=0;  k=0;
+    if(buf[i] != ' '){
+      noIndex[k++] = i;
+    }
+    while(buf[i] != '\0'){
+      i++;
+      if(buf[i-1] == ' ' && buf[i] != ' ' && buf[i] != '\n'){
+        noIndex[k++] = i;
+      }
+    }
+    if(buf[i-1] != '\n'){
+      fprintf(stderr, "Error:Too long input data\n");
+      exit(1);
+    }
+    //入力されたノードを格納して木を構築する
+    for(i=0;i<k;i++){
+      n = sscanf(&buf[ noIndex[i] ], "%d", &y);
+      if(n != 1){
+        fprintf(stderr, "Error:non-number input data\n");
+        continue;
+      }
+      if (i == 0) {//最初だけ根を構成する
+        q = (TREE *)malloc( sizeof(TREE) );
+        q->data = y;
+        q->left = NULL;
+        q->right = NULL;
+        root = q;
+      }else{//節や葉を構成する
+        p = root;
+        while(p != NULL){
+          if(y < p->data){//入れる値が今見ている値より小さいとき
+            if(p->left != NULL){//左が空いなかったら
+              p = p->left;//左へ移動
+            }
+            else{//空いていたら
+              q = (TREE *)malloc( sizeof(TREE) );
+              q->data = y;
+              q->left = NULL;
+              q->right = NULL;
+              p->left = q;//新しく葉(節)にする
+              break;
+            }
+          }
+          else if(y > p->data){
+            if(p->right != NULL){
+              p = p->right;
+            }
+            else{
+              q = (TREE *)malloc( sizeof(TREE) );
+              q->data = y;
+              q->left = NULL;
+              p->right = NULL;
+              p->right = q;
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void print_tree(int depth, TREE *p){
+  int i;
+  if(p == NULL){
+    return;
+  }
+  //木のデータを出力
+  print_tree(depth+1,p->right);
+  for(i=0;i<depth;i++){
+    fprintf(stdout, "    ");
+  }
+  fprintf(stdout," %4d\n", p->data);
+  print_tree(depth+1, p->left);
+}
+
+void insert_node(int y, TREE *p){
+  TREE *q;
+  //空でないか確認
+  if(p == NULL){
+    fprintf(stderr, "Error:There is no data!");
+    return;
+  }
+  q = (TREE *)malloc( sizeof(TREE) );
+  q->data = y;
+  q->left = NULL;
+  q->right = NULL;
+  if(y < p->data){
+    if(p->left != NULL){
+      insert_node(y,p->left);
+    }else{
+      q = (TREE *)malloc( sizeof(TREE) );
+      q->data = y;
+      q->left = NULL;
+      q->right = NULL;
+      p->left = q;
+    }
+  }
+  else if(y > p->data){
+    if(p->right != NULL){
+      insert_node(y,p->right);
+    }else{
+      q = (TREE *)malloc( sizeof(TREE) );
+      q->data = y;
+      q->left = NULL;
+      q->right = NULL;
+      p->right = q;
+    }
+  }
+}
+
+int delete_node(int y){//左側の最大値から求める
+  TREE *n, *p_n, *l_b;//自分　親　左の子
+  int d;
+  n = root;
+  p_n = NULL;
+  d = 0;
+  //空でないか確認
+  if(n == NULL){
+    fprintf(stderr, "Error:There is no data!");
+    return -1;
+    while(n != NULL && n->data != y){
+      if(n->data > y){//今の値より子が小さければ
+        p_n = n;//次の親となる
+        n = n->left;//左の子に移動
+        d = -1;
+      }
+      else{//子が大きければ
+        p_n = n;
+        n = n->right;
+        d = 1;
+      }
+    }
+    if(d==0){
+      fprintf(stderr,"Error:Serch data not found!\n");
+      return 0;
+    }
+    //削除するプログラム
+    if(n->left == NULL && n->right == NULL ){//葉なら
+      if(d == -1){
+        p_n->left = NULL;
+      }
+      else if(d == 1){
+        p_n->right = NULL;
+      }
+    }
+    else if(0){//一つの子を持つ親なら
+
+    }
+    else if(0){//二人の子を持つ親なら
+
+    }
+
+
+    return 1;
+  }
+
+
+
+
+}
+
+int main(){
+  int y;
+  root = NULL;
+  construct_tree();
+  print_tree(0,root);
+  //insert data
+  fprintf(stdout, "*******Insert_node*******\n");
+  fprintf(stdout, "Input insert data: ");
+  scanf("%d",&y);
+  insert_node(y,root);
+  print_tree(0,root);
+  //delete data
+  fprintf(stdout,"*******Delete*******\n");
+  fprintf(stdout,"Input delete-location: ");
+  scanf("%d",&y);
+  delete_node(y);
+  print_tree(0,root);
+
+  return 0;
+}
